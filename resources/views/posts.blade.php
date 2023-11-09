02f32d0a08a733e62255406a286b7ec90e717ab3
Daffa Arkantaqqy <isupply@Daffa-Arkantaqqy.local>
2023-11-09 20:49:04 +0700

    Mengerjakan Tugas Aplikasi Laravel

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+
+
+@extends('layouts.main')
+
+@section('container')
+    
+    @foreach ($posts as $post)
+    <article class="mb-4">
+    <h2>
+        <a href="/posts/{{ $post["slug"] }}">{{$post["title"]}}</a>
+    </h2>
+    <h5>By:{{$post["author"]}}</h5>
+    <p>{{$post["body"]}}</p>
+    </article>
+    @endforeach
+
+    
+
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,71 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view ('about', [
+        "title" => "About",
+        "name" => "Daffa Arkantaqqy",
+        "email" => "daffaarkantaqqy@gmail.com",
+        "image" => "foto.png"
+    ]);
+});
+
+
+Route::get('/blog', function () {
+    $blog_post = [
+        [
+            "title" => "Judul post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Daffa Arkantaqqy",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi consectetur accusantium ipsa eum placeat! Commodi laboriosam quibusdam dicta necessitatibus unde, cumque quia ipsum eveniet molestias nihil, repudiandae ex magnam illum natus quos aliquid nisi dignissimos harum labore numquam. Nemo dolore necessitatibus obcaecati ipsum dignissimos, perferendis pariatur magnam deserunt quos. Repudiandae praesentium voluptatum, dolorum, odio tenetur alias dolorem eveniet similique ad rem laboriosam quaerat vero nihil quam eos quae fugit reprehenderit magni cum. Alias saepe soluta et fugiat laboriosam, quidem modi!"
+        ],
+        [
+            "title" => "Judul post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Muhammad Rizki",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia earum ducimus ratione accusantium nostrum fuga, quae atque, iusto quis nihil excepturi quod tempora consectetur. Quia, repudiandae. Dolore dolorem accusamus earum? Exercitationem sequi cupiditate, molestiae consectetur facere saepe laudantium! Voluptatibus tempore, cupiditate odio ex incidunt repellat praesentium obcaecati! Accusantium doloremque perspiciatis rem ipsum, eum sint in autem architecto, quos natus sequi nihil! Aperiam, dicta dolore labore optio obcaecati laudantium quasi enim aliquid officia id ad quos, porro quaerat harum. Ut quam illum quisquam eum officia soluta saepe alias non, velit ducimus doloremque ipsa obcaecati dolorum vel accusamus sapiente. Iusto, fugiat quod! Laboriosam vitae dolorum architecto cupiditate praesentium ipsa, iusto repellendus fuga!",
+        ],
+    ];
+    return view ('posts', [
+        "title" => "Posts",
+        "posts" => $blog_post
+    ]);
+});
+
+
+
+
+//halaman single post
+Route::get('posts/{slug}', function($slug){
+    $blog_post = [
+        [
+            "title" => "Judul post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Daffa Arkantaqqy",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi consectetur accusantium ipsa eum placeat! Commodi laboriosam quibusdam dicta necessitatibus unde, cumque quia ipsum eveniet molestias nihil, repudiandae ex magnam illum natus quos aliquid nisi dignissimos harum labore numquam. Nemo dolore necessitatibus obcaecati ipsum dignissimos, perferendis pariatur magnam deserunt quos. Repudiandae praesentium voluptatum, dolorum, odio tenetur alias dolorem eveniet similique ad rem laboriosam quaerat vero nihil quam eos quae fugit reprehenderit magni cum. Alias saepe soluta et fugiat laboriosam, quidem modi!"
+        ],
+        [
+            "title" => "Judul post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Muhammad Rizki",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia earum ducimus ratione accusantium nostrum fuga, quae atque, iusto quis nihil excepturi quod tempora consectetur. Quia, repudiandae. Dolore dolorem accusamus earum? Exercitationem sequi cupiditate, molestiae consectetur facere saepe laudantium! Voluptatibus tempore, cupiditate odio ex incidunt repellat praesentium obcaecati! Accusantium doloremque perspiciatis rem ipsum, eum sint in autem architecto, quos natus sequi nihil! Aperiam, dicta dolore labore optio obcaecati laudantium quasi enim aliquid officia id ad quos, porro quaerat harum. Ut quam illum quisquam eum officia soluta saepe alias non, velit ducimus doloremque ipsa obcaecati dolorum vel accusamus sapiente. Iusto, fugiat quod! Laboriosam vitae dolorum architecto cupiditate praesentium ipsa, iusto repellendus fuga!",
+        ],
+    ];
+
+
+    $new_post = [];
+    foreach($blog_post as $post) {
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
